9bab67a9727b52faa323eb7239cd10116a323b7d
Chris Mieger <94837732+chmgr@users.noreply.github.com>
2022-12-07 23:00:05 +0100

    Adapted for laravel 9

--- src/MailFileDriver.php
+++ src/MailFileDriver.php
@@ -2,26 +2,41 @@
 
 namespace Mmeyer2k\LaravelMailFile;
 
+use Carbon\Carbon;
 use Illuminate\Mail\Transport\Transport;
 use Swift_Mime_SimpleMessage;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\RawMessage;
 
-class MailFileDriver extends Transport
+class MailFileDriver implements TransportInterface
 {
     /**
      * {@inheritdoc}
      *
      * @return int
      */
-    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
+	public function send(RawMessage $message, Envelope $envelope = null): ?SentMessage
     {
-        $time = number_format(microtime(true), '4', '.', '');
+        $time = Carbon::now()->format('Y-m-d_H-i-s_u');
 
         $base = env('MAIL_FILE_PATH') ?? sys_get_temp_dir();
 
-        $file = "$base/$time.txt";
+	    if (!file_exists($base))
+		    mkdir($base, 0777, true);
+		
+        $file = "$base/$time.eml";
 
         file_put_contents($file, $message->toString());
 
-        return 0;
+	    return new SentMessage($message, $envelope ?? Envelope::create($message));
     }
+	
+
+	public function __toString(): string {
+		return 'file';
+	}
+
+
 }
